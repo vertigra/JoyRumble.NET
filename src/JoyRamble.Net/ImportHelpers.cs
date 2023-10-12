32336599f9c9688da8bace1975d5b725e58bb7b6
Alexey Nesterov <vertigra.developer@gmail.com>
2023-10-12 15:17:18 +1000

    Init commit.

---
+++ src/JoyRamble.Net/ImportHelpers.cs
@@ -0,0 +1,8 @@
+﻿using System.Runtime.InteropServices;
+
+internal static class ImportHelpers
+{
+
+    [DllImport("joyrumble.so", EntryPoint = "joyrumble")]
+    public static extern int Joyrumble(int joynumber, int strong, int weak, int duration);
+}
---
+++ src/JoyRamble.Net/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.InteropServices;
+
+namespace JoyRumble.NET
+{
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ImportHelpers.Joyrumble(0, 8000, 500, 10);
+        }
+    }
+}
