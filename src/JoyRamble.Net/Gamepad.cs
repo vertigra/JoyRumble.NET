7fe41eb3cb9820b81621e7566a69813115105096
Alexey Nesterov <vertigra.developer@gmail.com>
2023-10-13 22:47:32 +1000

    Change app type. Change .net => netstandart

---
+++ src/JoyRamble.Net/Gamepad.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Cryptography;
+using System;
+using System.Threading.Tasks;
+
+namespace JoyRumble.NET
+{
+    public class Gamepad
+    {
+        public void Joyrumble(int joynumber, int strong, int weak, int duration)
+        {
+            ImportHelpers.Joyrumble(joynumber, strong, weak, duration);
+        }
+
+        public Task JoyrumbleAsync(int joynumber, int strong, int weak, int duration)
+        {
+            return Task.Run(() => ImportHelpers.Joyrumble(joynumber, strong, weak, duration));
+        }
+    }
+}
