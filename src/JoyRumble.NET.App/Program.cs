406b98fca19ccbca25f02479ff42958a1b0e51c7
Alexey Nesterov <vertigra.developer@gmail.com>
2023-10-14 21:00:31 +1000

    Closed issue #8
    * Update dotnet.yml
    
    * Update dotnet.yml
    
    * Fix issue #8
    
    * Change in build configuration

---
+++ src/JoyRumble.NET.App/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace JoyRumble.NET.App
+{
+    internal class Program
+    {
+        private static int mJoynumber, mStrong, mWeak, mDuration;
+        private static Gamepad mGamepad;
+        private static bool mRunAsync;
+
+        static void Main(string[] args)
+        {
+            mJoynumber = int.Parse(args[0]);
+            mStrong = int.Parse(args[1]);
+            mWeak = int.Parse(args[2]);
+            mDuration = int.Parse(args[3]);
+            mRunAsync = bool.Parse(args[4]);
+
+            mGamepad = new Gamepad();
+
+            if (mRunAsync)
+            {
+                StartAsync();
+                return;
+            }
+
+            mGamepad.Joyrumble(mJoynumber, mStrong, mWeak, mDuration);    
+        }
+
+        private static async void StartAsync()
+        {
+            await mGamepad.JoyrumbleAsync(mJoynumber, mStrong, mWeak, mDuration);
+        }
+    }
+}
