fe9bc5f84814e4451c5bab4d18f3cf22427356d2
Alexey Nesterov <vertigra.developer@gmail.com>
2023-10-16 15:46:43 +1000

    Change in build script

--- src/BuildLinuxDebug.sh
+++ src/BuildLinuxDebug.sh
@@ -1,4 +1,8 @@
-#!/bin/sh
-mv JoyRumble.Native/_project.json JoyRumble.Native/project.json
-dotnet build /p:Configuration=LinuxDebug JoyRumble.NET.sln
-mv JoyRumble.Native/project.json JoyRumble.Native/_project.json
+#!/usr/bin/env bash
+
+# Absolute path this script is in, thus /home/user/bin
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+mv $SCRIPT_DIR/JoyRumble.Native/_project.json $SCRIPT_DIR/JoyRumble.Native/project.json
+dotnet build /p:Configuration=LinuxDebug $SCRIPT_DIR/JoyRumble.NET.sln
+mv  $SCRIPT_DIR/JoyRumble.Native/project.json  $SCRIPT_DIR/JoyRumble.Native/_project.json
