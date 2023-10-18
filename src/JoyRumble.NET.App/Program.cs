730e232d76c39c90f2d025a07aa5480bec79eedc
Alexey Nesterov <vertigra.developer@gmail.com>
2023-10-18 20:49:36 +1000

    Close issue #17

--- src/JoyRumble.NET.App/Program.cs
+++ src/JoyRumble.NET.App/Program.cs
@@ -3,34 +3,16 @@
     internal class Program
     {
         private static int mJoynumber, mStrong, mWeak, mDuration;
-        private static Gamepad? mGamepad;
-        private static bool mRunAsync;
-
+        
         static void Main(string[] args)
         {
             mJoynumber = int.Parse(args[0]);
             mStrong = int.Parse(args[1]);
             mWeak = int.Parse(args[2]);
             mDuration = int.Parse(args[3]);
-            mRunAsync = bool.Parse(args[4]);
 
-            mGamepad = new Gamepad();
-
-            if (mRunAsync)
-            {
-                StartAsync();
-                return;
-            }
-
-            mGamepad.Joyrumble(mJoynumber, mStrong, mWeak, mDuration);    
-        }
-
-        private static async void StartAsync()
-        {
-            if (mGamepad == null)
-                return;
-
-            await mGamepad.JoyrumbleAsync(mJoynumber, mStrong, mWeak, mDuration);
+            Gamepad.Joyrumble(mJoynumber, mStrong, mWeak, mDuration);
+            Console.ReadLine();
         }
     }
 }
