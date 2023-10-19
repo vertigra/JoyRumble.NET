13c8100e0166434f253afef179167a806d717d29
Alexey Nesterov <vertigra.developer@gmail.com>
2023-10-19 16:26:31 +1000

    Fix issue #27 part 1

--- src/JoyRamble.Net/ImportHelpers.cs
+++ src/JoyRamble.Net/ImportHelpers.cs
@@ -3,6 +3,6 @@
 internal static class ImportHelpers
 {
 
-    [DllImport("joyrumble.so", EntryPoint = "joyrumble")]
+    [DllImport("joyrumble.arm64.so", EntryPoint = "joyrumble")]
     public static extern int Joyrumble(int joynumber, int strong, int weak, int duration);
 }
